629db49074cb9a2117f9b607c093e5f0543f26a7
Stanislas Lejay <stanislas.lejay@epita.fr>
2015-12-15 08:40:49 +0100

    [LEVEL3] libhooks rewritten

--- src/level3/memory_hooks.c
+++ src/level3/memory_hooks.c
@@ -5,71 +5,13 @@
 #include "shared.hh"
 // http://elinux.org/images/b/b5/Elc2013_Kobayashi.pdf
 
-static __thread int no_hook;
-
-static void* (*callocp)(size_t, size_t) = NULL;
-static void* (*mallocp)(size_t) = NULL;
-static void* (*reallocp)(void*, size_t) = NULL;
-static void  (*freep)(void*) = NULL;
-
-static void __attribute__((constructor)) init(void)
+void* malloc(size_t size)
 {
-        callocp   = (void* (*) (size_t, size_t)) dlsym (RTLD_NEXT, "calloc");
-        mallocp   = (void* (*) (size_t))         dlsym (RTLD_NEXT, "malloc");
-        reallocp  = (void* (*) (void *, size_t)) dlsym (RTLD_NEXT, "realloc");
-        freep     = (void  (*) (void *))         dlsym (RTLD_NEXT, "free");
-}
-
-void *malloc (size_t len)
-{
-        void *ret;
-
-        if (no_hook)
-                return (*mallocp)(len);
-
-        no_hook = 1;
-        fprintf(stdout, "malloc(%zu", len);
-        ret = (*mallocp)(len);
-        asm volatile ("int3" : : "a" (CUSTOM_SYSCALL_MALLOC));
-        fprintf(stdout, ") -> %p\n", ret);
-        no_hook = 0;
-        return ret;
-}
-
-void *calloc (size_t n, size_t len)
-{
-        void *ret;
-        if (no_hook)
-        {
-                if (callocp == NULL)
-                {
-                        //ret = my_calloc(n, len);
-                        ret = calloc(n, len);
-                        return ret;
-                }
-        }
-        no_hook = 1;
-        ret = (*callocp)(n, len);
+        static void* (*my_malloc)(size_t) = NULL;
+        printf("inside shared object...\n");
+        if (!my_malloc)
+                my_malloc = dlsym(RTLD_NEXT, "malloc");  /* returns the object reference for malloc */
+        void *p = my_malloc(size);               /* call malloc() using function pointer my_malloc */
         asm volatile ("int3" : : "a" (CUSTOM_SYSCALL_CALLOC));
-        no_hook = 0;
-        return ret;
-}
-
-
-void *realloc (void* ptr, size_t len)
-{
-        no_hook = 1;
-        void* ret = (*reallocp)(ptr, len);
-        asm volatile ("int3" : : "a" (CUSTOM_SYSCALL_REALLOC));
-        no_hook = 0;
-        return ret;
-}
-
-
-void free (void* ptr)
-{
-        no_hook = 1;
-        (*freep)(ptr);
-        asm volatile ("int3" : : "a" (CUSTOM_SYSCALL_FREE));
-        no_hook = 0;
+        return p;
 }
